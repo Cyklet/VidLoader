3404234e61f381921891c74f9ca8329bd8df03fb
Petre Plotnic <11734269+Cyklet@users.noreply.github.com>
2020-07-05 11:56:06 +0200

    Update download documentation (#16)

--- VidLoader/VidLoader/Sources/Models/DownloadValues.swift
+++ VidLoader/VidLoader/Sources/Models/DownloadValues.swift
@@ -14,11 +14,11 @@ public struct DownloadValues {
     let minRequiredBitrate: Int?
     
     /// - Parameters:
-    ///   - identifier: Item unique identifier
+    ///   - identifier: Item's unique identifier
     ///   - url: Stream URL
-    ///   - title: Item title that will be presented in the phone settings
-    ///   - artworkData: Item thumbnail that will be presented in the phone settings
-    ///   - minRequiredBitrate: Lowest media bitrate to be used that is greater than or equal to this value, bits per second.  If nil then the highest media bitrate will be selected by default.
+    ///   - title: Item's title that will be presented in the phone settings
+    ///   - artworkData: Item's optional thumbnail that will be presented in the phone settings
+    ///   - minRequiredBitrate: Lowest media bitrate to be used that is greater than or equal to this value, bits per second. If it's nil, then the highest media bitrate will be selected by default.
     public init(identifier: String, url: URL, title: String, artworkData: Data? = nil, minRequiredBitrate: Int? = nil) {
         self.identifier = identifier
         self.url = url
